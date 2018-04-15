f08df0426e6231b0d7ec92dfc5aeecd7920e7b8c
Alexandre Proulx <alexandre.proulx.3@gmail.com>
2018-04-15 11:00:28 -0400

    no message

--- Sherweb.HackDon.Api/Program.cs
+++ Sherweb.HackDon.Api/Program.cs
@@ -13,7 +13,7 @@ namespace Sherweb.HackDon.Api
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://*:5000;")
+                .UseUrls("http://0.0.0.0:5000")
                 .Build();
     }
 }
